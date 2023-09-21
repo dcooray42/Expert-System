30518037159decb710b8a3aa891948b823f3b18d
dcooray42 <dcooray@student.42.fr>
2023-09-21 19:16:51 +0200

    Ajout de l'inference engine et du backward chain pratiquement fini

--- engine.py
+++ engine.py
@@ -1,27 +1,21 @@
-def evaluate_expression(expression, known_facts) :
+def evaluate_expression(es, expression) :
     stack = []
 
-    for token in expression:
-        if isinstance(token, FactNode) :
-            # If it's a FactNode, push its value onto the stack
-            stack.append(token.value)
-        elif token in "+|!^" :
-            # If it's an operator, apply it to the operands on the stack
+    for token in expression :
+        if token.isalpha() :
+            stack.append(token)
+        else :
+            operand = backward_chain(es, stack.pop())
+            if token in "+|^" :
+                operand_bis = backward_chain(es, stack.pop())
             if token == '+' :
-                operand2 = stack.pop()
-                operand1 = stack.pop()
-                result = operand1 and operand2
+                result = operand_bis and operand
             elif token == '|' :
-                operand2 = stack.pop()
-                operand1 = stack.pop()
-                result = operand1 or operand2
+                result = operand_bis or operand
             elif token == '!' :
-                operand = stack.pop()
                 result = not operand
             elif token == '^' :
-                operand2 = stack.pop()
-                operand1 = stack.pop()
-                result = operand1 != operand2  # XOR is not equal
+                result = operand_bis != operand  # XOR is not equal
             stack.append(result)
 
     # The result should be on the top of the stack
@@ -29,15 +23,27 @@ def evaluate_expression(expression, known_facts) :
 
 
 def backward_chain(es, query) :
-    if es.known_facts[query].check == True :
-        return es.known_fact[query].value
+    def val_to_check(es, conclusion, value) :
+        for token in conclusion :
+            if token.isalpha() :
+                if es.facts[token].value != value and es.facts[token].check == True :
+                    return False
+        return True
+
+    def update_facts(es, conclusion, value) :
+        for token in conclusion :
+            if token.isalpha() :
+                es.facts[token].value = value
+                es.facts[token].check = True
+
+    if es.facts[query].check == True :
+        return es.facts[query].value
 
     for rule in es.rules :
         if query in rule.conclusion and len(set(es.initial_facts).intersection(set(rule.conclusion))) == 0 :
-            # non termine a partir d'ici
-            is_rule_valid = evaluate_expression(rule.expression, inference_engine.known_facts)
-            if is_rule_valid :
-                inference_engine.known_facts[goal] = True
-                return True
+            result = evaluate_expression(es, rule.condition)
+            if val_to_check(es, rule.conlusion, result) :
+                update_facts(es, rule.conclusion, result)
+                return result
 
     return False
