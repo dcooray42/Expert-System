f37c5e02d5df56297214504d49fa7cc233cf648a
Dimitri Cooray <dcooray@student.42.fr>
2023-10-12 14:11:51 +0200

    Suppression d'un print inutile dans main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def main():
     args = vars(args)
     read_file(es, **args)
     for query in es.queries:
-        print(f"asdfoiuhoidsfiouh query : {query.fact} {len(query.rules)}")
         backward_chain(query)
     print("Queries:")
     for query in es.queries:
